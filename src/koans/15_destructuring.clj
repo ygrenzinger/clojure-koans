054be991f52cc39ee33131ba35d6d6340adf8805
ygrenzinger <yannick.grenzinger@gmail.com>
2016-02-28 22:06:35 +0100

    Clojure koans done

---
+++ src/koans/15_destructuring.clj
@@ -0,0 +1,56 @@
+(ns koans.15-destructuring
+  (:require [koan-engine.core :refer :all]))
+
+(def test-address
+  {:street-address "123 Test Lane"
+   :city "Testerville"
+   :state "TX"})
+
+(defn display-full-name [[first-name last-name]]
+  (str first-name " " last-name))
+
+(defn display-address [address]
+  (let [{:keys [street-address city state]} test-address]
+       (apply str (interpose ", " [street-address city state]))))
+
+(defn display-person [full-name address]
+  (str (display-full-name full-name) ", " (display-address address)))
+
+(meditations
+  "Destructuring is an arbiter: it breaks up arguments"
+  (= ":bar:foo" ((fn [[a b]] (str b a))
+         [:foo :bar]))
+
+  "Whether in function definitions"
+  (= (str "An Oxford comma list of apples, "
+          "oranges, "
+          "and pears.")
+     ((fn [[a b c]] (str "An Oxford comma list of " a ", " b ", and " c "." ))
+      ["apples" "oranges" "pears"]))
+
+  "Or in let expressions"
+  (= "Rich Hickey aka The Clojurer aka Go Time aka Lambda Guru"
+     (let [[first-name last-name & aliases]
+           (list "Rich" "Hickey" "The Clojurer" "Go Time" "Lambda Guru")]
+       (str first-name " " last-name " aka " (apply str (interpose " aka " aliases)))))
+
+  "You can regain the full argument if you like arguing"
+  (= {:original-parts ["Stephen" "Hawking"] :named-parts {:first "Stephen" :last "Hawking"}}
+     (let [[first-name last-name :as full-name] ["Stephen" "Hawking"]]
+       {:original-parts [first-name last-name] :named-parts {:first first-name :last last-name}}))
+
+  "Break up maps by key"
+  (= "123 Test Lane, Testerville, TX"
+     (let [{street-address :street-address, city :city, state :state} test-address]
+       (apply str (interpose ", " [street-address city state]))))
+
+  "Or more succinctly"
+  (= "123 Test Lane, Testerville, TX"
+     (let [{:keys [street-address city state]} test-address]
+       (apply str (interpose ", " [street-address city state]))))
+
+
+
+  "All together now!"
+  (= "Test Testerson, 123 Test Lane, Testerville, TX"
+     (display-person ["Test" "Testerson"] test-address)))
